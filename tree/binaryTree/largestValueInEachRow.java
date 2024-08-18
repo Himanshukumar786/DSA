90b71690f4998982bdc5e5e7eb515ad1fc944ed8
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-08-18 16:39:20 +0530

    code added

---
+++ tree/binaryTree/largestValueInEachRow.java
@@ -0,0 +1,38 @@
+package tree.binaryTree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+public class largestValueInEachRow {
+     public List<Integer> largestValues(TreeNode root) {
+        List<Integer> answer = new ArrayList<>();
+
+        if(root == null) return answer;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while(q.size() > 0){
+            int size = q.size();
+            int ans = Integer.MIN_VALUE;
+            // List<Integer> ans = new ArrayList<>();
+            while(size-- > 0){
+                TreeNode temp = q.remove(); //got poped
+                if(temp.left != null){
+                    q.add(temp.left);
+                }
+                if(temp.right != null){
+                    q.add(temp.right);
+                }
+                
+                ans = Math.max(ans, temp.val);
+            }
+            answer.add(ans);
+        }
+        return answer;
+    }
+}
---
+++ tree/binaryTree/sumRootToLeaf.java
@@ -0,0 +1,28 @@
+package tree.binaryTree;
+
+import javax.swing.tree.TreeNode;
+
+public class sumRootToLeaf {
+    class Solution {
+        int ans=0;
+        
+        public int sum(TreeNode root, int num){
+            if(root.left == null && root.right == null){
+                ans += 10 * num + root.val;
+            }
+            int n = 10 * num + root.val;
+            if(root.left != null){
+                sum(root.left, n);
+            }
+            if(root.right != null){
+                sum(root.right, n);
+            }
+            return ans;
+        }
+        
+        public int sumNumbers(TreeNode root) {
+            sum(root, 0);
+            return ans;
+        }
+    }
+}
