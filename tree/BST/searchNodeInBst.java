f20fb7a63ca2160511add286aac356649acdca15
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-08-24 02:02:33 +0530

    code added

---
+++ tree/BST/searchNodeInBst.java
@@ -0,0 +1,18 @@
+package tree.BST;
+
+import javax.swing.tree.TreeNode;
+
+public class searchNodeInBst {
+    public TreeNode searchBST(TreeNode root, int val) {
+        
+        if(root == null) return null;
+        // for finding the value
+        if(root.val == val) return root;
+        
+        TreeNode leftsubtree = searchBST(root.left, val);
+        TreeNode rightsubtree = searchBST(root.right, val);
+        
+        // which subtree is not null is returned
+        return rightsubtree != null  ? rightsubtree : leftsubtree;
+    }
+}
