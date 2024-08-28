c08694e843ab8252732006db1fdd3b49d5001426
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-08-29 00:25:04 +0530

    code

---
+++ tree/binaryTree/rootEqualSumOfChilds.java
@@ -0,0 +1,12 @@
+package tree.binaryTree;
+
+import javax.swing.tree.TreeNode;
+
+public class rootEqualSumOfChilds {
+        class Solution {
+        // simple straight forward que -> value of root == sum of their childs value
+        public boolean checkTree(TreeNode root) {
+            return (root.val) == (root.left.val) + (root.right.val);
+        }
+    }
+}
