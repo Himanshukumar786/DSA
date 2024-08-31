7538ff7fa259da6ba88b9b5003aeb38964dcb84a
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-09-01 00:32:33 +0530

    code

---
+++ tree/BST/LCA_0f_BST.java
@@ -0,0 +1,16 @@
+package tree.BST;
+
+import javax.swing.tree.TreeNode;
+
+public class LCA_0f_BST {
+    // simple BST approach 
+    // other way is we can separate checks(if) conditions for root , p , and q.
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // going left
+        if(root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
+        // going right
+        else if(root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+        // if above both condition is not true that means p and q is root or root equals to null.
+        else return root;
+    } 
+}
