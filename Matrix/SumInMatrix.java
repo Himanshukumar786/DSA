f972993509195ada8d9c3ebe18b3278d176726b4
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-09-09 01:25:32 +0530

    code added

---
+++ Matrix/SumInMatrix.java
@@ -0,0 +1,24 @@
+package Matrix;
+
+import java.util.Arrays;
+
+public class SumInMatrix {
+      public int matrixSum(int[][] nums) {
+        int m = nums.length;
+        int n = nums[0].length;
+        int result = 0;
+        
+         
+        for(int i = 0; i<m; i++){
+            Arrays.sort(nums[i]);
+        }
+        for (int j = 0; j < n; j++) {
+            int temp = 0;
+            for (int i = 0; i < m; i++){
+                temp = Math.max(temp, nums[i][j]);
+            }
+            result += temp;
+        }
+        return result;
+    }
+}
