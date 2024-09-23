5188f285800386bfe62bc8996396f00a4276b0b9
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-09-23 23:43:12 +0530

    code added

--- LinkedList/RemoveNth_node_from_endOf_LL.java
+++ LinkedList/RemoveNth_node_from_endOf_LL.java
@@ -1,5 +1,29 @@
 package LinkedList;
 
 public class RemoveNth_node_from_endOf_LL {
-    
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head == null) return head;
+        
+        ListNode fast = head ;
+        int i = 0; 
+        while(i < n && fast != null){
+            i++;
+            fast = fast.next;
+        }
+        
+        if(fast == null){
+            ListNode newhead = head.next;
+            head.next = null;
+            return newhead;
+        }
+        
+        ListNode slow = head;
+        while(fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        
+        return head;
+    }
 }
