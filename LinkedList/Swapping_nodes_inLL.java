ae0e9aa33acc0d99a334da29626fa886fded209a
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-09-25 20:28:29 +0530

    added codes

---
+++ LinkedList/Swapping_nodes_inLL.java
@@ -0,0 +1,21 @@
+package LinkedList;
+
+public class Swapping_nodes_inLL {
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode fast = head;
+        for (int i = 1; i < k; i++) {
+            fast = fast.next;
+        }
+        ListNode p = fast;
+        ListNode slow = head;
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        ListNode q = slow;
+        int t = p.val;
+        p.val = q.val;
+        q.val = t;
+        return head;
+    }
+}
