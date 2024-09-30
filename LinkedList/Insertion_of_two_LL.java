0a5ba27d829923a26ca30c731c77e49252bfee25
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-09-30 23:17:02 +0530

    Added code

---
+++ LinkedList/Insertion_of_two_LL.java
@@ -0,0 +1,12 @@
+package LinkedList;
+
+public class Insertion_of_two_LL {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA, b = headB;
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        return b;
+    }
+}
