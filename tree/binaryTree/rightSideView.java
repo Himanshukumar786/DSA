2a6b2e893f8cb6f12fc87c923f553be1a2b77013
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-10-12 23:22:41 +0530

    code

--- tree/binaryTree/rightSideView.java
+++ tree/binaryTree/rightSideView.java
@@ -8,27 +8,33 @@ import java.util.Queue;
 import javax.swing.tree.TreeNode;
 
 public class rightSideView {
-        class Solution {
+    class Solution {
         public List<Integer> rightSideView(TreeNode root) {
+
             // normal bfs
             List<Integer>answer = new ArrayList<>();
 
             if(root == null) return answer;
 
             Queue<TreeNode> q = new LinkedList<>();
+
             q.add(root);
+
             while(!q.isEmpty()){
                 int size = q.size();
                 while(size > 0){
                     TreeNode temp = q.remove(); 
+
                     if(temp.left != null){
                         q.add(temp.left);
                     }
                     if(temp.right != null){
                         q.add(temp.right);
                     }
+
                     // main code to get right view
                     size--;
+
                     if(size == 0){
                         answer.add(temp.val);
                     }
