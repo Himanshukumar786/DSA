a53925742e50be0e2c9fc34b5229fe83b7410e6a
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-10-16 23:21:46 +0530

    code

--- LinkedList/palindrome.java
+++ LinkedList/palindrome.java
@@ -3,8 +3,7 @@ package LinkedList;
 public class palindrome {
     class Solution {
         public boolean isPalindrome(ListNode head) {
-            ListNode slow = head;
-            ListNode fast = head;
+            ListNode slow , fast = head;
             ListNode prev = null;
             ListNode nhead = null;
             
@@ -12,19 +11,24 @@ public class palindrome {
                 prev = slow;
                 slow = slow.next;
                 fast = fast.next.next;
+                
                 if(nhead == null){
                     nhead = new ListNode(prev.val);
                 } else {
                     ListNode node = new ListNode(prev.val);
+
                     node.next = nhead;
                     nhead = node;
                 }
             }
+
             // if length of ll is odd that means fast reaches at the last node not the null
+
             if(fast != null){
                 slow = slow.next;
             }
             while(slow != null && nhead != null){
+
                 if(slow.val != nhead.val) return false;
                 
                 nhead = nhead.next;
