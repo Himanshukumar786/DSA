3585bf4b0a4db901bc8086fe33d66b402a996efb
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-10-16 23:20:00 +0530

    code

--- Strings/FindIndex_of_first_occurrence.java
+++ Strings/FindIndex_of_first_occurrence.java
@@ -1,5 +1,9 @@
 package Strings;
 
 public class FindIndex_of_first_occurrence {
-    
+    class Solution {
+        public int strStr(String haystack, String needle) { // String inbuilt method -> indecOf
+            return haystack.indexOf(needle);            
+        }
+    }
 }
