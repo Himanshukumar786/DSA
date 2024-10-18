43dfcbaeff8588092c370c7c207f23d418364e17
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-10-18 23:42:22 +0530

    Added code

--- Strings/Is_Subsequence.java
+++ Strings/Is_Subsequence.java
@@ -1,5 +1,21 @@
 package Strings;
 
 public class Is_Subsequence {
-    
+    class Solution {
+        public boolean isSubsequence(String s, String t) {
+            
+            int m = s.length();
+            int n = t.length();
+            
+            int i = 0, j = 0;
+            
+            while (i < m && j < n) {
+                if(s.charAt(i) == t.charAt(j)) {
+                    i++;
+                }
+                j++;
+            }
+            return i == m;
+        }
+    }
 }
