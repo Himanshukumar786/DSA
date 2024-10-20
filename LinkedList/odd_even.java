401ef3e78275a9a3c2e5429976e406b47eb81a06
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-10-20 23:01:08 +0530

    code

--- LinkedList/odd_even.java
+++ LinkedList/odd_even.java
@@ -3,6 +3,7 @@ package LinkedList;
 public class odd_even {
     class Solution {
         public ListNode oddEvenList(ListNode head) {
+            
             if(head == null || head.next == null) return head;
             
             ListNode odd = head;
@@ -11,7 +12,9 @@ public class odd_even {
             
             // in this question while continue is imp bcs odd even kidhar rokna hai yeh figure out krna hoga
             // and odd condition phle check hogi even after
+            
             while(odd.next != null && even.next != null){
+
                 odd.next = even.next;
                 odd = odd.next;
                 
