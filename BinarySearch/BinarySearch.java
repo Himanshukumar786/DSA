60229af29c44af6710b45090c36fc9ef26bcd217
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-10-27 21:39:44 +0530

    code

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -1,23 +1,33 @@
 package BinarySearch;
 
 public class BinarySearch {
-    public int search(int[] nums, int target) {
-        
-        // binary serach implementation
-        
-        int lo = 0;
-        int hi = nums.length - 1;
-        
-        while( lo <= hi){
-            
-            int mid = lo + (hi - lo)/2;
-            
-            if(nums[mid] == target) return mid;
-            
-            else if(nums[mid] > target) hi = mid - 1;
-            
-            else if(nums[mid] < target) lo = mid + 1;
+    public static int binarySearch(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
+
         return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 10, 40};
+        int target = 10;
+        int result = binarySearch(arr, target);
+        if (result == -1) {
+            System.out.println("Element not present");
+        } else {
+            System.out.println("Element found at index " + result);
+        }
     }
 }
