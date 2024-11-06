9a26bc81e33997ff41813b16b0b2bb6d0d724642
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-11-07 01:02:20 +0530

    code

--- TwoPointer/SortColor.java
+++ TwoPointer/SortColor.java
@@ -5,6 +5,7 @@ public class SortColor {
         int temp;
         for(int i=0;i<nums.length-1;i++){
             for(int j=i+1;j<nums.length;j++){
+
                 if(nums[i]>nums[j]){
                     temp=nums[i];
                     nums[i]=nums[j];
