9d337855a24e61e071bae0173a697724f785e070
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-11-11 23:55:23 +0530

    code

--- bit_manipulation/single_number.java
+++ bit_manipulation/single_number.java
@@ -3,8 +3,10 @@ package bit_manipulation;
 public class single_number {
     class Solution {
         public int singleNumber(int[] nums) {
+
             // using the XOR (bitwise XOR)
             // ex => 1^1 = 0 , 1 ^ 2 = 1
+            
             int value = 0;
             
             for(int i = 0; i < nums.length; i++){
