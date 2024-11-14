03d71416c6ade84e203b55d969cc739b9ae00847
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-11-14 23:52:14 +0530

    code

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -23,7 +23,9 @@ public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {2, 3, 4, 10, 40};
         int target = 10;
+        
         int result = binarySearch(arr, target);
+
         if (result == -1) {
             System.out.println("Element not present");
         } else {
