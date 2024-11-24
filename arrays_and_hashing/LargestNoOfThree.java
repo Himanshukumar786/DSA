451adb4a3d580db92193a793b87328ea4429d357
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-11-24 22:05:51 +0530

    code

--- arrays_and_hashing/LargestNoOfThree.java
+++ arrays_and_hashing/LargestNoOfThree.java
@@ -5,71 +5,79 @@ class Solution {
         int n = digits.length;
         int sum = 0;
         
-        for(int i = 0; i < n; i++) sum += digits[i];
+        for (int i = 0; i < n; i++) sum += digits[i];
         
         Arrays.sort(digits);
-        if(sum % 3 == 0){
-            if(digits[n - 1] == 0) return "0";
-            String str = "";
-            for(int i = n - 1; i >= 0; i--){
-                str += "" + digits[i];
+        if (sum % 3 == 0) {
+            if (digits[n - 1] == 0) return "0";
+            StringBuilder str = new StringBuilder();
+            for (int i = n - 1; i >= 0; i--) {
+                str.append(digits[i]);
             }
-            return str;
-        } else if(sum % 3 == 1){
+            return str.toString();
+        } else if (sum % 3 == 1) {
             boolean idealFound = false;
-            for(int i = 0; i < n; i++){
-                if(digits[i] % 3 == 1){
+            for (int i = 0; i < n; i++) {
+                if (digits[i] % 3 == 1) {
                     idealFound = true;
                     digits[i] = -1;
                     break;
                 }
             }
-            if(!idealFound){
+            if (!idealFound) {
                 int[] idx = new int[2];
                 idx[0] = -1;
                 idx[1] = -1;
 
-                for(int i = 0; i < n; i++){
-                    if(digits[i] % 3 == 2){
-                        if(idx[0] == -1) idx[0] = i;
-                        else idx[1] = i; break;
+                for (int i = 0; i < n; i++) {
+                    if (digits[i] % 3 == 2) {
+                        if (idx[0] == -1) idx[0] = i;
+                        else {
+                            idx[1] = i;
+                            break;
+                        }
                     }
                 }
-                if(idx[1] != -1){
+                if (idx[1] != -1) {
                     digits[idx[0]] = -1;
                     digits[idx[1]] = -1;
                 } else return "";
             }
         } else {
             boolean idealFound = false;
-            for(int i = 0; i < n; i++){
-                if(digits[i] % 3 == 2){
+            for (int i = 0; i < n; i++) {
+                if (digits[i] % 3 == 2) {
                     idealFound = true;
                     digits[i] = -1;
                     break;
                 }
             }
-            if(!idealFound){
+            if (!idealFound) {
                 int[] idx = new int[2];
                 idx[0] = -1;
                 idx[1] = -1;
 
-                for(int i = 0; i < n; i++){
-                    if(digits[i] % 3 == 1){
-                        if(idx[0] == -1) idx[0] = i;
-                        else idx[1] = i; break;
+                for (int i = 0; i < n; i++) {
+                    if (digits[i] % 3 == 1) {
+                        if (idx[0] == -1) idx[0] = i;
+                        else {
+                            idx[1] = i;
+                            break;
+                        }
                     }
                 }
-                if(idx[1] != -1){
+                if (idx[1] != -1) {
                     digits[idx[0]] = -1;
                     digits[idx[1]] = -1;
                 } else return "";
             }
         }
-        String ans = "";
-        for(int i = n - 1; i >= 0; i--){
-            if(digits[i] != -1) ans += ("" + digits[i]);
+        StringBuilder ans = new StringBuilder();
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] != -1) ans.append(digits[i]);
         }
-        return ans;
+        if (ans.length() == 0) return "";
+        if (ans.charAt(0) == '0') return "0";
+        return ans.toString();
     }
 }
