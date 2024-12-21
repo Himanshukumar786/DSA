3914e0c071a331bf30b746ab0b1ee9019074de60
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-12-22 01:55:56 +0530

    code

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -23,6 +23,7 @@ public class BinarySearch {
     public static void main(String[] args) {
         
         int[] arr = {2, 3, 4, 10, 40};
+        
         int target = 10;
         
         int result = binarySearch(arr, target);
