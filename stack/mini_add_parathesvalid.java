d9b983195afe8824be7fc72e22b0cd830309e67d
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2024-12-26 23:07:50 +0530

    code

--- stack/mini_add_parathesvalid.java
+++ stack/mini_add_parathesvalid.java
@@ -9,6 +9,7 @@ public class mini_add_parathesvalid {
         public int minAddToMakeValid(String s) {
             
             Stack<Character> st = new Stack<>();
+            
             for(int i = 0; i < s.length(); i++){
                 if(s.charAt(i) == '('){
                     st.push(s.charAt(i));
