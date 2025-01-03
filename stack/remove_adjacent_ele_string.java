0247574b4e229083a6da4a70883b6d95c13d93ab
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2025-01-03 14:25:47 +0530

    code

--- stack/remove_adjacent_ele_string.java
+++ stack/remove_adjacent_ele_string.java
@@ -4,26 +4,26 @@ import java.util.Stack;
 
 public class remove_adjacent_ele_string {
     class Solution {
-    public String removeDuplicates(String s) {
-        Stack<Character> st = new Stack<>();
-        
-        for(int i = 0; i <= s.length() - 1; i++){
-            if(st.size() == 0){
-                st.push(s.charAt(i));
-                continue;
+        public String removeDuplicates(String s) {
+            Stack<Character> st = new Stack<>();
+            
+            for(int i = 0; i <= s.length() - 1; i++){
+                if(st.size() == 0){
+                    st.push(s.charAt(i));
+                    continue;
+                }
+                if(st.peek() == s.charAt(i)){
+                    st.pop();
+                } else {
+                    st.push(s.charAt(i));
+                }
             }
-            if(st.peek() == s.charAt(i)){
-                st.pop();
-            } else {
-                st.push(s.charAt(i));
+            
+            char[] arr = new char[st.size()];
+            for(int i = arr.length-1; i >= 0; i--){
+                arr[i] = st.pop();
             }
+            return new String(arr);
         }
-        
-        char[] arr = new char[st.size()];
-        for(int i = arr.length-1; i >= 0; i--){
-            arr[i] = st.pop();
-        }
-        return new String(arr);
     }
-}
 }
