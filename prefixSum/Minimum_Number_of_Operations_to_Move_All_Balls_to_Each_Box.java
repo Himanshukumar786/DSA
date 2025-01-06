8c079545f5edc7889348251ee660c8212ebbd906
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2025-01-06 17:49:10 +0530

    added code

--- prefixSum/Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.java
+++ prefixSum/Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.java
@@ -3,7 +3,6 @@ package prefixSum;
 import java.util.HashSet;
 
 public class Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box {
-    
     // TC -> O(N^N)
     // SC -> O(N)
 
@@ -25,6 +24,26 @@ public class Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box {
                     ans[i] += Math.abs(i - idx);
                 }
             }
+            return ans;
+        }
+    }
+
+    // TC -> O(N^N)
+    // SC -> O(1)
+
+    class Solution2 {
+        public int[] minOperations(String boxes) {
+            int n = boxes.length();
+            int[] ans = new int[n];
+
+            for(int i = 0; i < n; i++){
+                if(boxes.charAt(i) == '1'){
+                    for(int j = 0; j < n; j++){
+                        ans[j] += Math.abs(j - i);
+                    }
+                }
+            }
+
             return ans;
         }
     }
