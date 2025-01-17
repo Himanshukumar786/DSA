d41256b5a34aed65703973da193afdd7938e1fc1
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2025-01-17 23:41:51 +0530

    code

--- arrays_and_hashing/mimimumCommom_value.java
+++ arrays_and_hashing/mimimumCommom_value.java
@@ -10,11 +10,13 @@ public class mimimumCommom_value {
         for(int i=0;i<nums1.length;i++){
             s.add(nums1[i]);
         }
+
         for(int j=0;j<nums2.length;j++){
             if(s.contains(nums2[j])){
                 return nums2[j];
             }
         }
+        
         return -1;
     }
 }
