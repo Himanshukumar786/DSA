912126278272cba976a4b1ee402846c7535bfb5f
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2025-01-30 03:03:02 +0530

    code

--- prefixSum/Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.java
+++ prefixSum/Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.java
@@ -37,7 +37,9 @@ public class Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box {
             int[] ans = new int[n];
 
             for(int i = 0; i < n; i++){
+
                 if(boxes.charAt(i) == '1'){
+                    
                     for(int j = 0; j < n; j++){
                         ans[j] += Math.abs(j - i);
                     }
