14c14133e17a0025b2414363357af762728c9d9c
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2025-02-10 17:00:54 +0530

    added code

--- Matrix/Matrix_Diagonal_Sum.java
+++ Matrix/Matrix_Diagonal_Sum.java
@@ -1,5 +1,19 @@
 package Matrix;
 
 public class Matrix_Diagonal_Sum {
+    class Solution {
+        public int diagonalSum(int[][] mat) {
+            int n = mat.length;
+            int sum = 0;
+            
+            for (int i = 0; i < n; i++) {
+                sum += mat[i][i]; 
     
+                if (i != n - 1 - i) {
+                    sum += mat[i][n - 1 - i]; 
+                }
+            }
+            return sum;
+        }
+    }
 }
