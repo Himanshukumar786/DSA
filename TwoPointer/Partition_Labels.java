0205b7d32d5d60c64a28391f24abf0dba16dacff
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2025-04-01 02:19:04 +0530

    code

---
+++ TwoPointer/Partition_Labels.java
@@ -0,0 +1,33 @@
+package TwoPointer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Partition_Labels {
+    class Solution {
+        public List<Integer> partitionLabels(String s) {
+            int n = s.length();
+            List<Integer> ans = new ArrayList<>();
+            int[] map = new int[26];
+
+            for(int i = 0; i < n; i++){
+                // int index = s.charAt(i) - 'a';
+                // map[index] = i;
+                map[s.charAt(i) - 'a'] = i;
+            }
+
+            int i = 0;
+            while(i < n){
+                int end = map[s.charAt(i) - 'a'];
+                int j = i;
+                while(j < end){
+                    end = Math.max(end, map[s.charAt(j) - 'a']);
+                    j++;
+                }
+                ans.add(j - i + 1);
+                i = j + 1;
+            }
+            return ans;
+        }
+    }
+}
